f154ab3770f70c4a7d8b58d2f928eac571016809
Rameshvlp <rameshvlp2001@gmail.com>
2023-10-08 00:14:05 +0530

    Improved ui

--- SCANNER/App.js
+++ SCANNER/App.js
@@ -1,115 +1,22 @@
+// App.js
+import React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+import ScannerScreen from './ScannerScreen';
+import DisplayScreen from './DisplayScreen';
 
-import React, { useState, useEffect } from 'react';
-import { Text, View, StyleSheet, Button } from 'react-native';
-import { BarCodeScanner } from 'expo-barcode-scanner';
-import { firebase } from './config';
+const Stack = createStackNavigator();
 
-export default function App() {
-  const [hasPermission, setHasPermission] = useState(null);
-  const [scanned, setScanned] = useState(false);
-  const [isScanning, setIsScanning] = useState(false);
-  const [text, setText] = useState('Not yet scanned');
-  const [name, setName] = useState('');
-
-  const askForCameraPermission = async () => {
-    const { status } = await BarCodeScanner.requestPermissionsAsync();
-    setHasPermission(status === 'granted');
-  };
-
-  useEffect(() => {
-    askForCameraPermission();
-  }, []);
-
-  const handleBarCodeScanned = async ({ type, data }) => {
-    setScanned(true);
-    const scannedData = data.split('\n');
-    const [name, email, department, college, event] = scannedData;
-
-    console.log('Name:', name);
-    console.log('Email:', email);
-    console.log('Department:', department);
-    console.log('College:', college);
-    console.log('Event:', event);
-
-    try {
-      const docRef = await firebase.firestore().collection('new-data').add({
-        name,
-        email,
-        department,
-        college,
-        event,
-        timestamp: firebase.firestore.FieldValue.serverTimestamp(),
-      });
-      console.log('Document written with ID:', docRef.id);
-    } catch (error) {
-      console.error('Error adding document:', error);
-    }
-    
-    setText(data);
-  };
-
-  if (hasPermission === null) {
-    return (
-      <View style={styles.container}>
-        <Text>Requesting for camera permission</Text>
-      </View>
-    );
-  }
-  if (hasPermission === false) {
-    return (
-      <View style={styles.container}>
-        <Text style={{ margin: 10 }}>No access to camera</Text>
-        <Button title={'Allow Camera'} onPress={() => askForCameraPermission()} />
-      </View>
-    );
-  }
-
-  const startScanning = () => {
-    setIsScanning(true);
-    setScanned(false);
-  }
-
+const App = () => {
   return (
-    <View style={styles.container}>
-      {!isScanning ? (
-        <View>
-          <Text style={styles.maintext}>Click the button to start scanning</Text>
-          <Button title={'Start Scanning'} onPress={() => startScanning()} />
-        </View>
-      ) : (
-        <View>
-          <View style={styles.barcodebox}>
-            <BarCodeScanner
-              onBarCodeScanned={scanned ? undefined : handleBarCodeScanned}
-              style={{ height: 400, width: 400 }} />
-          </View>
-          <Text style={styles.maintext}>{text}</Text>
-          {scanned && <Button title={'Scan again?'} onPress={() => setScanned(false)} color='tomato' />}
-          <Button title={'Back to Get Started'} onPress={() => setIsScanning(false)} />
-        </View>
-      )}
-    </View>
-  );
-}
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName="Scanner">
+        <Stack.Screen name="Welcome To PSG TECH" component={ScannerScreen} />
+        <Stack.Screen name="DisplayScreen" component={DisplayScreen} />
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-  maintext: {
-    fontSize: 16,
-    margin: 20,
-  },
-  barcodebox: {
-    alignItems: 'center',
-    justifyContent: 'center',
-    height: 300,
-    width: 300,
-    overflow: 'hidden',
-    borderRadius: 30,
-    backgroundColor: 'tomato'
-  }
-});
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+};
+
+export default App;
--- SCANNER/config.js
+++ SCANNER/config.js
@@ -4,13 +4,13 @@ import 'firebase/compat/firestore';
 
 const firebaseConfig={
     
-    apiKey: "AIzaSyDj5ZpVo_HKxmCehkd-OpXiPjmGulTDOcY",
-  authDomain: "qr-registree-40be3.firebaseapp.com",
-  projectId: "qr-registree-40be3",
-  storageBucket: "qr-registree-40be3.appspot.com",
-  messagingSenderId: "733968466445",
-  appId: "1:733968466445:web:323596faeb881484400b44",
-  measurementId: "G-63MCM4LFFS"
+  apiKey: "AIzaSyDGj4Gw3pucthISZ_lahzPO60gw983FvOc",
+  authDomain: "qr-registree-mad.firebaseapp.com",
+  projectId: "qr-registree-mad",
+  storageBucket: "qr-registree-mad.appspot.com",
+  messagingSenderId: "284270592487",
+  appId: "1:284270592487:web:37be55b76a6eee50483d42",
+  measurementId: "G-8R3SDTT8FK"
 };
 
 if(!firebase.apps.length){
